0518521e25129e09483f0ef9468fd7be1082d4ec
Hugo Prol <hprol@innovacs.es>
2016-10-10 23:31:54 +0200

    Convert octave code to matlab and minor changes

--- week1/ExtractSignalFeatures.m
+++ week1/ExtractSignalFeatures.m
@@ -7,37 +7,37 @@ function [features signs] = ExtractSignalFeatures(dir)
   % - dir: directory with the dataset (jpeg files + gt/ + mask/)
   %
   % Output:
-  % - features: matrix Nx4, with the features of each sign.
+  % - features: Nx4 matrix with the features of each sign.
   %             Columns: height, width, form factor and filling ratio.
-  % - signs:    matrix Nx1, with the type of each sign.
+  % - signs:    character vector with the type of each sign.
   %
-  addpath(genpath("."));
+  addpath(genpath('.'));
   
   features = [];
-  signs = [];
+  signs = '';
 
   files = ListFiles(dir);
 
   for i = 1:size(files, 1)
-    name = strtrunc(files(i).name, 9);
-    txt = strcat(dir, "/gt/gt.", name, ".txt");
-    png = strcat(dir, "/mask/mask.", name, ".png");
+    name = files(i).name(1:9);
+    txt = strcat(dir, '/gt/gt.', name, '.txt');
+    png = strcat(dir, '/mask/mask.', name, '.png');
 
     gt = imread(png);
     [window, type] = LoadAnnotations(txt);
 
     for j = 1:size(window, 1)
-      x1 = int64(window(j).x);
-      y1 = int64(window(j).y);
-      x2 = int64(window(j).x + window(j).w);
-      y2 = int64(window(j).y + window(j).h);
+      x1 = round(window(j).x);
+      y1 = round(window(j).y);
+      x2 = round(window(j).x + window(j).w);
+      y2 = round(window(j).y + window(j).h);
       mask = gt(y1:y2, x1:x2);
 
       filling_ratio = sum(mask(:)) / numel(mask);
       form_factor = window(j).w / window(j).h;
 
       features = [features; window(j).h, window(j).w, form_factor, filling_ratio];
-      signs = [signs; type{j+1}];
+      signs = [signs; type{j}];
     end
   end
 end
