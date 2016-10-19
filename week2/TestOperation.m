feb7ad15f5d229a70eb95532b4b128d7c2761b46
jordi-bird <jordi.puyoles.calafell@gmail.com>
2016-10-19 21:44:46 +0000

    Test task 2

--- week2/TestOperation.m
+++ week2/TestOperation.m
@@ -1,18 +1,17 @@
 function [timeMatlab, timeCustom] = TestOperation(operation,testImg,structel,struCent)
 
-if operation == 'Dilation'
+if operation == 'D'
   matStrel = strel ('square',size(structel,1));
   tic
-  imdilate(testImg,structel);
+  imdilate(testImg,matStrel);
   timeMatlab = toc;
   tic
   M1_Team8_Dilation(testImg,structel,struCent);
   timeCustom = toc;
-end
-elseif operation == 'Erosion'
+elseif operation == 'E'
   matStrel = strel ('square',size(structel,1));
   tic
-  imerode(testImg,structel);
+  imerode(testImg,matStrel);
   timeMatlab = toc;
   tic
   M1_Team8_Erosion(testImg,structel,struCent);
--- week2/makeTest.m
+++ week2/makeTest.m
@@ -1,4 +1,60 @@
-function makeTest(outFile)
+function makeTest(outFileName)
 
-outFile=fopen('CustomVsMatlab_MorphologicalMethods');
+outFile=fopen(outFileName,'w');
 
+TheImage=imread('cameraman.tif');
+
+TheStructel= ones(3);
+
+TheCenter=[2,2];
+
+for i=1:40
+    fprintf(outFile,'-');
+end
+fprintf(outFile,'\n');
+fprintf(outFile,'Dilation');
+fprintf(outFile,'\n');
+for i=1:40
+    fprintf(outFile,'-');
+end
+fprintf(outFile,'\n');
+
+[timeMatlab, timeCustom] = TestOperation('D',TheImage,TheStructel,TheCenter);
+
+fprintf(outFile,'Matlab:');
+
+fprintf(outFile,num2str(timeMatlab));
+
+fprintf(outFile,' seconds\n');
+
+fprintf(outFile,'Custom:');
+
+fprintf(outFile,int2str(timeCustom));
+
+fprintf(outFile,' seconds\n');
+
+
+for i=1:40
+    fprintf(outFile,'-');
+end
+fprintf(outFile,'\n');
+fprintf(outFile,'Erosion');
+fprintf(outFile,'\n');
+for i=1:40
+    fprintf(outFile,'-');
+end
+fprintf(outFile,'\n');
+
+[timeMatlab, timeCustom] = TestOperation('E',TheImage,TheStructel,TheCenter);
+
+fprintf(outFile,'Matlab:');
+
+fprintf(outFile,num2str(timeMatlab));
+
+fprintf(outFile,' seconds\n');
+
+fprintf(outFile,'Custom:');
+
+fprintf(outFile,num2str(timeCustom));
+
+fprintf(outFile,' seconds\n');
