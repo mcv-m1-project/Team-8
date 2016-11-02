c64215103523bd76f5620734ebf027ec628984b8
jordi-bird <jordi.puyoles.calafell@gmail.com>
2016-11-02 20:48:43 +0000

    color model generation added

--- week3/task3_test.m
+++ week3/task3_test.m
@@ -1,13 +1,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Generates masks and bounding boxes from test directory for submission
 %using a simple sliding window
+
+addpath(genpath('.'))
+%Directory to save the resulting masks (png) and bounding boxes (mat)
+output_dir = '/home/ihcv08/m1-results/week3/test/integral_results';
+% Directory train
+train_dir = '/home/ihcv08/dataset/trial3/puretrain/';
+%Directory with images to analyze
+test_dir = '/home/ihcv08/dataset/test';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Parameter to be tuned for color model generation
 backproj_thr = 0.022;
 saturation_thr = 0.3;
-%Directory with images to analyze
-test_dir = '/home/ihcv08/dataset/test';
-%Directory to save the resulting masks (png) and bounding boxes (mat)
-output_dir = '/home/ihcv08/m1-results/week3/test/integral_results';
+
+% Create color model for the pixel detection stage
+[hc, hl, cl] = ExtractHistograms(train_dir);
+[color_model, lum_model] = ComputeColorModel(hc, hl, cl);
+
+
 %Sliding window parameters:
 %
 %step_w: window step. Distance in pixels to avoid neighbour
@@ -16,9 +30,7 @@ step_w = 8;
 %
 %fr_threshold: min filling ratio to consider a window contains object
 fr_threshold = 0.3;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-addpath(genpath('.'))
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Perform test
 TrafficSignWindowIntegral_test(test_dir, ...
--- week3/task5_test.m
+++ week3/task5_test.m
@@ -1,13 +1,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Generates masks and bounding boxes from test directory for submission
 %using a simple sliding window
+
+addpath(genpath('.'))
+%Directory to save the resulting masks (png) and bounding boxes (mat)
+output_dir = '/home/ihcv08/m1-results/week3/test/conv_results';
+% Directorir train
+train_dir = '/home/ihcv08/dataset/trial3/puretrain/';
+%Directory with images to analyze
+test_dir = '/home/ihcv08/dataset/test';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Parameter to be tuned for color model generation
 backproj_thr = 0.022;
 saturation_thr = 0.3;
-%Directory with images to analyze
-test_dir = '/home/ihcv08/dataset/test';
-%Directory to save the resulting masks (png) and bounding boxes (mat)
-output_dir = '/home/ihcv08/m1-results/week3/test/conv_results';
+
+% Create color model for the pixel detection stage
+[hc, hl, cl] = ExtractHistograms(train_dir);
+[color_model, lum_model] = ComputeColorModel(hc, hl, cl);
+
+
+
 %Sliding window parameters:
 %
 %step_w: window step. Distance in pixels to avoid neighbour
